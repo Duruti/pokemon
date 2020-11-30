6153deee178544075ce646bd876e7433cbae5924
duruti <duruti43@gmail.com>
2020-11-30 18:43:27 +0100

    add card

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
+// Element du DOM
+let ul = document.querySelector('.containerGrid')
+
 let allPokemon = [];
+// grass: '#78c850',
+// ground: '#E2BF65',
+// dragon: '#6F35FC',
+// fire: '#F58271',
+// electric: '#F7D02C',
+// fairy: '#D685AD',
+// poison: '#966DA3',
+// bug: '#B3F594',
+// water: '#6390F0',
+// normal: '#D9D5D8',
+// psychic: '#F95587',
+// flying: '#A98FF3',
+// fighting: '#C25956',
+//  rock: '#B6A136',
+//  ghost: '#735797',
+//  ice: '#96D9D6'
+const color = {
+   red : '#F58271',
+   blue : '#6390F0',
+   yellow : '#F7D02C',
+   green : '#78c850',
+   black : '#000',
+   brown : '#E2BF65',
+   purple : '#7151c2',
+   gray : '#D9D5D8',
+   white : '#f1f1f1',
+   pink : '#D685AD',
+
+}
 
 getPokemon();
 //console.log(allPokemon)
 
 async function getPokemon(){
 
-   let result =  await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1`)
+   let result =  await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
       .then(json => json.json())
       .then(data => {
          data.results.forEach(element => {
             getStat(element.url)
          });
       })
-   
+   console.log(result)
 }
 
 
@@ -24,6 +56,7 @@ async function getStat(url){
    
    let o = {
       id : result.id,
+      color : '',
       name : result.name,
       types : [],
       pathImg : result.sprites.front_default
@@ -41,4 +74,45 @@ async function getStat(url){
    let language = "ja"
    let frenchName = species.names.filter( array => array.language.name === language)
    console.log(frenchName[0].name)
+   o.color = species.color.name
+   // creation de l'élément 
+   let card = document.createElement('li')
+   card.style.backgroundColor = color[o.color]
+   card.classList.add('card')
+   
+   // nom du pokemon
+   let name = document.createElement('h1')
+   name.innerText = o.name
+   card.appendChild(name)
+   
+   // id
+   let idContainer = document.createElement('div');
+   idContainer.classList.add('id');
+   let idText = document.createElement('p');
+   idText.innerText = o.id
+   idContainer.appendChild(idText);
+   card.appendChild(idContainer)
+
+   // sprite
+   let sprite = document.createElement('img')
+   sprite.classList.add('sprite')
+   sprite.src = o.pathImg
+   card.appendChild(sprite)
+   
+   // type
+
+   let types = document.createElement('div');
+   types.classList.add('type');
+   for (let i=0 ; i<o.types.length ; i++){
+      let type = document.createElement('p');
+      type.innerText = o.types[i]
+      console.log(o.types[i])
+      types.appendChild(type);
+      
+   }
+   card.appendChild(types)
+
+
+   ul.appendChild(card)
+
 }
